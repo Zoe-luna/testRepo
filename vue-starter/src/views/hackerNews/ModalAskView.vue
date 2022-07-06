b332a9451021bb137a0c6e2b3a56a1ae02642343
Zoe-luna <arcasarin@naver.com>
2022-07-06 15:18:33 +0900

    7/6 news page 수정완료

---
+++ vue-starter/src/views/hackerNews/ModalAskView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <section>
+      <div class="user-container">
+        <div>
+          <i class="fas fa-child fa-2x"></i>
+        </div>
+        <div class="user-description">
+            {{ modal.user }}
+          <div class="time">{{ modal.time_ago }}</div>
+        </div>
+      </div>
+      <h2>{{ modal.title }}</h2>
+    </section>
+    <section>
+      <div v-html="modal.content"></div>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+  name: 'ModalAsk',
+  components: {},
+  props: [
+      'modal',
+  ],
+  data() {
+    return {
+      item: {}
+    }
+  },
+}
+</script>
+
+<style scoped>
+.user-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+.user-description {
+  padding-left: 8px;
+}
+.time {
+  font-size: 0.7rem;
+}
+</style>
